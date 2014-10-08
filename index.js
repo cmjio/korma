7dc65f9bd852cea36decf55267bef6bf0406275f
Aditya <aditya@netroy.in>
2014-10-08 15:34:27 +0200

    cache  lookup

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-module.exports = function korma(func){
-  var args = Array.prototype.slice.call(arguments, 1);
+'use strict';
+
+var slice = Array.prototype.slice;
+
+module.exports = function korma(func) {
+  var args = slice.call(arguments, 1);
 
   return function() {
-    return func.apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
+    return func.apply(this, args.concat(slice.call(arguments, 0)));
   }
 };
